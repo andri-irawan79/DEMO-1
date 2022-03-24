60389450ac658f6109bfbcf05a8716bb58ba3dc9
andri-irawan79 <iandri647@gmail.com>
2022-03-24 16:47:42 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="text-black display-4 mb-5 ">Todo List</h1>
+    <InputField :todos="todos" :newTodo="newTodo"/>
+    <ListTodo :todos="todos" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputField from '@/components/InputField.vue'
+import ListTodo from '@/components/ListTodo.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      todos: [],
+      newTodo: '',
+      serialNumber: 0
+    };
+  },
   components: {
-    HelloWorld
+    InputField,
+    ListTodo,
   }
+  // methods: {
+  //   addTodos() {
+      
+  //   }
+  // }
 }
 </script>
 
@@ -19,6 +36,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  /* background-color: rgb(255, 255, 255); */
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/ListTodo.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container-xl w-100 my-5">
+        <table class="w-100 table-striped">
+            <tr class="text-black">
+                <th class="col-1">No.</th>
+                <th class="col-7">Todo</th>
+                <th class="col-4">Action</th> 
+            </tr>
+            <TodoItems :todoList="todoList" />     
+        </table>
+    </div>
+</template>
+
+<script>
+import TodoItems from '@/components/TodoItems.vue'
+
+export default {
+    name: 'ListTodo',
+    components: {
+        TodoItems,
+    },
+    props: {
+        todos: Array,
+    },
+    data() {
+        return {
+            todoList: this.todos,
+        };
+    }
+}
+</script>
+
+
+
+<style scoped>
+th,
+tr,
+td {
+    border: 1px solid rgb(168, 168, 168);
+    padding: 10px;
+}
+
+table tr th {
+    background-color: royalblue;
+}
+
+table tr:nth-child(odd) {
+    background-color: rgb(231, 233, 238);
+}
+</style>
+
+
+// methods: {
+//         deletTodo(index) {
+//             this.todoList.splice(index, 1);
+//         },
+//         doEdit(index){ 
+//             this.isEdit = true;
+//             this.editedTodo = this.todoList[index];
+//         },
+//         addEdit(index){
+//             this.todoList.splice(index, 1, this.editedTodo);
+//             this.isEdit = false;
+//         },
+//         changeButtonEdit() {
+//             return this.isEdit ? 'Done' : 'Edit';
+//         } 
+//     }
